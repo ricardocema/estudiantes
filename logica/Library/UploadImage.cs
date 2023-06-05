39dca730b15aada6f1ffe36ec4ffeb32fe8e59a4
Ricardo Jose Cedeño Mancilla <rcedeno@cic.uis.edu.co>
2023-06-05 18:00:37 -0500

    second commit

---
+++ logica/Library/UploadImage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logica.Library
+{
+    public class UploadImage
+    {
+        private OpenFileDialog fd = new OpenFileDialog();
+
+        public void CargarImagen(PictureBox pictureBox)
+        {
+            pictureBox.WaitOnLoad = true;
+            fd.Filter = "Imagenes | *.jpg;*.gif;*.png;*bmp";
+            fd.ShowDialog();
+
+            if (fd.FileName != string.Empty)
+            {
+                pictureBox.ImageLocation = fd.FileName;
+
+            }
+        }
+    }
+}
